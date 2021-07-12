31620af2dd7a5faece7ce725d1a12e9de6a5eeff
Marcos Vinicius <marcosvinicius.ccc@gmail.com>
2021-07-12 12:12:07 -0300

    ToDoList component

---
+++ src/components/ToDoList.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>Lista de tarefas</p>
+  <ul>
+    <li v-for="toDo in toDoList" :key="toDo.id">{{ toDo.title }}</li>
+  </ul>
+</template>
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  data: () => ({
+    toDoList: [{ id: 'asdasd', title: 'Teste' }],
+  }),
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Hello world to Vue app" />
+    <ToDoList />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { defineComponent } from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import ToDoList from '@/components/ToDoList.vue';
 
 export default defineComponent({
-  name: "Home",
+  name: 'Home',
   components: {
     HelloWorld,
+    ToDoList,
   },
 });
 </script>
